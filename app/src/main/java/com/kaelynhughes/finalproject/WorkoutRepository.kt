1cfe35cef49dca2509b591d17dba0787f2941ad8
Kaelyn Hughes <kaelynmhughes@gmail.com>
2022-12-09 20:16:45 -0700

    Repository set up

--- app/src/main/java/com/kaelynhughes/finalproject/WorkoutRepository.kt
+++ app/src/main/java/com/kaelynhughes/finalproject/WorkoutRepository.kt
@@ -4,34 +4,11 @@ import androidx.room.Room
 import com.kaelynhughes.finalproject.models.Workout
 
 object WorkoutRepository {
-    private val db: AppDatabase;
-    private var cacheInitialized = false;
-    private val workoutsCache = mutableListOf<Workout>()
     init {
-        db = Room.databaseBuilder(
+        Room.databaseBuilder(
             WorkoutsApplication.getInstance(),
             AppDatabase::class.java,
             "workouts-database"
         ).build()
-    }
-
-    suspend fun createWorkout(workout: Workout) {
-        workout.id = db.getWorkoutsDao().createWorkout(workout)
-        workoutsCache.add(workout)
-    }
-
-    suspend fun getAllWorkouts(): List<Workout> {
-        if (!cacheInitialized) {
-            workoutsCache.addAll(db.getWorkoutsDao().getAllWorkouts())
-            cacheInitialized = true
-        }
-        return workoutsCache
-    }
-
-    suspend fun update(workout: Workout) {
-        db.getWorkoutsDao().updateWorkout(workout)
-        workoutsCache[workoutsCache.indexOfFirst {
-            it.id == workout.id
-        }] = workout
     }
 }
