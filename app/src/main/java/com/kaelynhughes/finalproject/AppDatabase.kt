180913626b06422993eb4559b0ccd4090976e30a
Kaelyn Hughes <kaelynmhughes@gmail.com>
2022-12-09 22:35:23 -0700

    removed all commented-out code

--- app/src/main/java/com/kaelynhughes/finalproject/AppDatabase.kt
+++ app/src/main/java/com/kaelynhughes/finalproject/AppDatabase.kt
@@ -1,7 +1,6 @@
 package com.kaelynhughes.finalproject
 
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.kaelynhughes.finalproject.models.Workout
 
